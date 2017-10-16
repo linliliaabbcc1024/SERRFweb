76102a700c553ee548b0df6743205ec8f490d3c3
slfan2013 <fansili2013@gmail.com>
2017-10-16 10:49:01 -0700

    a

---
+++ LOCAL/run me.R
@@ -0,0 +1,10 @@
+# workingdirectory is in which folder your file is. 
+workingdirectory = "C:\\Users\\Sili Fan\\Downloads"
+# filename is the name of your file. Check out the example.xlsx for format requirements.
+filename = "example.xlsx"
+
+
+source("core.R")
+
+# PLEASE FEEL FREE TO EMAIL ME AT slfan@ucdavis.edu FOR ANY HELP. 
+# IF HELP NEEDED, PLEASE SEND ME DATA WITH ALL THE INFORMATION ENCODED SO THAT THE IMPORTANT INFORMATION IS BLINDED TO ME.
